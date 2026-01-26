0a94e654b60c3b17b8868d8afd921fe8e35a9aa3
Rajesh Ghosh <rajgaming688@gmail.com>
2026-01-26 16:27:20 +0530

    Done LB3

--- src/main/java/com/lovebabbar/dsa/array/FindKthMaxMin.java
+++ src/main/java/com/lovebabbar/dsa/array/FindKthMaxMin.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 public class FindKthMaxMin {
 
     public static void main(String[] args) {
-         int arr[] = {8, -3, 21, -4, 10};
+         int[] arr = {8, -3, 21, -4, 10};
          int k = 3;
 
+         //sort the array
         Arrays.sort(arr);
         int min = arr[k - 1];
         int max = arr[arr.length - k];
         System.out.println(max + " " + min);
     }
-    
 }
