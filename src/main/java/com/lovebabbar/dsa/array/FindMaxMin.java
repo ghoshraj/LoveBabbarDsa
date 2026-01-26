be4ea4ba53c5b42c3c338f58fb6b10b2792373b4
Rajesh Ghosh <rajgaming688@gmail.com>
2026-01-26 14:14:23 +0530

    Solve max min problem

--- src/main/java/com/lovebabbar/dsa/array/FindMaxMin.java
+++ src/main/java/com/lovebabbar/dsa/array/FindMaxMin.java
@@ -1,12 +1,26 @@
 package com.lovebabbar.dsa.array;
 
+import java.util.ArrayList;
+
 public class FindMaxMin {
 
     public static void main(String[] args) {
-        int arr[] = {8, -3, 21, -4, 10};
+        int[] arr = {8, -3, 21, -4, 10};
+
+        FindMaxMin obj = new FindMaxMin();
+        ArrayList<Integer> result = obj.getMinMax(arr);
+
+        System.out.println("Min value: " + result.get(0));
+        System.out.println("Max value: " + result.get(1));
+
+    }
+
+    public ArrayList<Integer> getMinMax(int[] arr) {
+        // code Here
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
 
+        //find min and max
         for(int i = 0; i < arr.length; i++){
             if (arr[i] > max) {
                 max = arr[i];
@@ -16,8 +30,13 @@ public class FindMaxMin {
             }
 
         }
-        System.out.println(max + " " + min);
 
+        //add the min and max in to the list and return it
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(min);
+        result.add(max);
+
+        return result;
     }
     
 }
