d069cb32241edaba9e66ca01aa3e7936d8d75768
Rajesh Ghosh <rajgaming688@gmail.com>
2026-01-27 19:47:24 +0530

    Union of 2sorted array is done

---
+++ src/main/java/com/lovebabbar/dsa/array/UnionOfTwoSortedArrays.java
@@ -0,0 +1,45 @@
+package com.lovebabbar.dsa.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnionOfTwoSortedArrays {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4};
+        int[] arr2 = {3, 4, 5, 6};
+
+        List<Integer> union = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+
+        // Main two-pointer loop
+        while (i < arr1.length && j < arr2.length){
+            if (arr1[i] < arr2[j]){
+                union.add(arr1[i]);
+                i++;
+            }
+            else if (arr1[i] > arr2[j]) {
+                union.add(arr2[j]);
+                j++;
+            } else {
+                union.add(arr1[i]);
+                i++;
+                j++;
+            }
+        }
+
+        // Remaining elements of arr1
+        while (i < arr1.length) {
+            union.add(arr1[i]);
+            i++;
+        }
+
+        // Remaining elements of arr2
+        while (j < arr2.length) {
+            union.add(arr2[j]);
+            j++;
+        }
+        System.out.println("union of 2 array is :" + union);
+    }
+    
+}
