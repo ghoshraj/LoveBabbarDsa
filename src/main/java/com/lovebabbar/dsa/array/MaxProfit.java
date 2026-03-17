9c6124d892c4df9f4341c4ec725198bcbee29e4b
Rajesh Ghosh <rajgaming688@gmail.com>
2026-03-17 11:58:53 +0530

    MaxProfit code is completed

---
+++ src/main/java/com/lovebabbar/dsa/array/MaxProfit.java
@@ -0,0 +1,25 @@
+package com.lovebabbar.dsa.array;
+
+public class MaxProfit {
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        int maxProfit = maxProfit(prices);
+        System.out.println("Max Profit: " + maxProfit);
+    }
+
+    public static int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+                if (prices[i] < minPrice) {
+                    minPrice = prices[i];
+                }
+                else {
+                    int profit = prices[i] - minPrice;
+                    if (profit > maxProfit) maxProfit = profit;
+                }
+            }
+        return maxProfit;
+    }
+}
