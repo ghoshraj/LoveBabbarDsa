15a5a54dbf7657b569aa7004b83a7ae5b79e6776
Rajesh Ghosh <rajgaming688@gmail.com>
2026-04-01 09:43:02 +0530

    print all subsequences code is done

---
+++ src/main/java/com/lovebabbar/dsa/string/Subsequence.java
@@ -0,0 +1,18 @@
+package com.lovebabbar.dsa.string;
+
+public class Subsequence {
+
+    public static void main(String[] args) {
+         String s = "ab";
+         printSubsequence(s, "", 0);
+    }
+
+    public static void printSubsequence(String s, String ans, int index){
+        if (index == s.length()) {
+            System.out.println(ans);
+            return;
+        }
+        printSubsequence(s, ans + s.charAt(index), index+ 1);
+        printSubsequence(s, ans, index + 1);
+    }
+}
