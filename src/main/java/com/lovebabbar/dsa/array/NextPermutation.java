1c8a612f3f0eb78bb45b064de1a935645c7a3dc7
Rajesh Ghosh <rajgaming688@gmail.com>
2026-04-05 12:03:12 +0530

    complete permutation code

--- src/main/java/com/lovebabbar/dsa/array/NextPermutation.java
+++ src/main/java/com/lovebabbar/dsa/array/NextPermutation.java
@@ -4,7 +4,7 @@ public class NextPermutation {
 
     public static void main(String[] args) {
 
-        int[] arr = {1, 2, 3};
+        int[] arr = {2, 4, 1, 7, 5, 0};
         int index = -1;
 
         for (int i = arr.length - 1; i > 0; i--) {
